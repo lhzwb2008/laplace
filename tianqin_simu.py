d12c7480fcb2ca93ee76570ea46d9fcea0defe35
清穹 <zark.zhangwb@alibaba-inc.com>
2023-11-23 00:44:09 +0800

    up

--- tianqin_simu.py
+++ tianqin_simu.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
+import os
+os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from keras.models import Sequential
+from keras.layers import LSTM, Dense,GRU,Dropout, Bidirectional
+from sklearn.preprocessing import MinMaxScaler
+from keras.utils import Sequence
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -13,81 +19,80 @@ data_clean = pd.read_csv("future_ss2312_tick.csv")
 
 # 1. 数据加载和预处理
 # data_clean = data.sort_values(by='trade_time')
+# 确保'close'列是数值型
 data_clean['close'] = pd.to_numeric(data_clean['close'], errors='coerce')
+
+for feature in price_features:
+    data_clean[feature + '_diff'] = data_clean['close'] - data_clean[feature]
+
 data_clean['trade_time'] = pd.to_datetime(data_clean['trade_time'])
-data_clean['volume'] = pd.to_numeric(data_clean['数量'], errors='coerce')
 
-# 2. 特征生成
-# Example usage:
-rolling_windows = {'mean': 300, 'std': 300, 'rsi': 400}
-ewm_spans = {'short': 200, 'long': 800, 'signal': 800}
+data_clean['close_diff'] = data_clean['close'].diff()
 
-# 滚动统计特征
-data_clean['rolling_mean'] = data_clean['close'].rolling(window=rolling_windows['mean']).mean()
-data_clean['rolling_std'] = data_clean['close'].rolling(window=rolling_windows['std']).std()
+# Define label
+data_clean['label'] = (data_clean['close'].shift(-100) > data_clean['close']).astype(int)
 
-
-# Calculate RSI
-delta = data_clean['close'].diff()
-gain = (delta.where(delta > 0, 0)).fillna(0)
-loss = (-delta.where(delta < 0, 0)).fillna(0)
-avg_gain = gain.rolling(window=rolling_windows['rsi']).mean()
-avg_loss = loss.rolling(window=rolling_windows['rsi']).mean()
-rs = avg_gain / avg_loss
-data_clean['RSI'] = 100 - (100 / (1 + rs))
-
-# Calculate MACD
-short_ema = data_clean['close'].ewm(span=ewm_spans['short'], adjust=False).mean()
-long_ema = data_clean['close'].ewm(span=ewm_spans['long'], adjust=False).mean()
-data_clean['MACD'] = short_ema - long_ema
-data_clean['MACD_signal'] = data_clean['MACD'].ewm(span=ewm_spans['signal'], adjust=False).mean()
-
-# Shift RSI and MACD to use them as features for next timestep
-data_clean['RSI_shifted'] = data_clean['RSI'].shift(1)
-data_clean['MACD_shifted'] = data_clean['MACD'].shift(1)
-data_clean['MACD_signal_shifted'] = data_clean['MACD_signal'].shift(1)
-
-# 累积成交量特征
-# 计算单位时间内的成交量变化
-data_clean['volume_change'] = data_clean['volume'].diff()
-
-# 累积成交量特征生成
-data_clean['volume_rolling_mean'] = data_clean['volume'].rolling(window=300).mean()
-
-
-# 价格变化率和成交量变化
-data_clean['price_change_rate'] = data_clean['close'].pct_change()
-
-# 定义标签
-data_clean['label'] = (data_clean['close'].shift(-1000) > data_clean['close']).astype(int)
-
+features = ['close_diff', '数量'] + [f + '_diff' for f in price_features]
 
 # 3. 分割数据
-# 分割数据为训练集和测试集
-# Convert the 'trade_time' column to datetime
-data_clean['trade_time'] = pd.to_datetime(data_clean['trade_time'])
 
 # Now you can filter the data between two dates
-train_data = data_clean[(data_clean['trade_time'] >= '2023-09-20 09:00:00') & 
-                        (data_clean['trade_time'] < '2023-10-15 09:00:00')]
+train_data = data_clean[(data_clean['trade_time'] >= '2023-09-01 09:00:00') & 
+                        (data_clean['trade_time'] < '2023-10-25 09:00:00')]
 
-test_data = data_clean[(data_clean['trade_time'] >= '2023-10-15 09:00:00') & 
-                        (data_clean['trade_time'] < '2023-10-20 09:00:00')]
-
-
-features = ['close', 'rolling_mean', 'rolling_std', 'RSI', 'MACD', 'volume_rolling_mean', 'price_change_rate', 'volume_change']
+test_data = data_clean[(data_clean['trade_time'] >= '2023-10-25 09:00:00') & 
+                        (data_clean['trade_time'] < '2023-10-31 09:00:00')]
 
 
-X_train = train_data[features].dropna()
-y_train = train_data['label'][X_train.index]
-X_test = test_data[features].dropna()
-y_test = test_data['label'][X_test.index]
+# 初始化归一化器
+scaler = MinMaxScaler(feature_range=(0, 1))
 
-# 4. 模型训练
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+train_data[features] = scaler.fit_transform(train_data[features])
 
-model.fit(X_train, y_train)# Use the cleaned data for training
 
+# 将 DataFrame 转换为 NumPy 数组
+X_train = np.array(train_data[features])
+y_train = np.array(train_data['label'])
+
+# 删除 NaN 值
+mask = ~np.isnan(X_train).any(axis=1)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+# 首先，确保 X_train 和 X_test 没有 NaN 值
+X_train = X_train[~np.isnan(X_train).any(axis=1)]
+y_train = y_train[~np.isnan(X_train).any(axis=1)]
+
+class TimeseriesGenerator(Sequence):
+    def __init__(self, data, labels, length, stride=1, batch_size=32):
+        self.data = data
+        self.labels = labels
+        self.length = length
+        self.stride = stride
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil((len(self.data) - self.length) / float(self.stride * self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_x = []
+        batch_y = []
+
+        start = idx * self.batch_size * self.stride
+        end = start + self.batch_size * self.stride + self.length
+
+        for i in range(start, min(end, len(self.data) - self.length), self.stride):
+            batch_x.append(self.data[i: i + self.length])
+            batch_y.append(self.labels[i + self.length])
+
+        return np.array(batch_x), np.array(batch_y)
+
+# 定义时间步长和步长
+time_steps = 300
+stride = 1  # 增加步长以减少内存使用
+
+from keras.models import load_model
+model = load_model('model_lstm_300.h5')
 
 #实时预测
 import pandas as pd
@@ -132,95 +137,80 @@ ticks = api.get_tick_serial("SHFE.ss2312")
 
 
 
-def predict_next_move(tick, model, rolling_windows, ewm_spans, historical_data):
-    # 将 'last_price' 作为 'current' 进行计算
+def predict_next_move(tick, model, time_steps,historical_data,scaler):
     tick['close'] = tick['last_price']
-    
+    tick['昨收盘'] = tick['pre_close']
+    tick['今开盘'] = tick['open']
+    tick['最高价'] = tick['highest'] 
+    tick['最低价'] = tick['lowest']
+    tick['申买价一'] = tick['bid_price1']
+    tick['申卖价一'] = tick['ask_price1']
+    tick['数量'] = tick['volume']
     # 将新的 tick 数据追加到历史数据中
     historical_data = pd.concat([historical_data, pd.DataFrame([tick])], ignore_index=True)
-    
-    # 打印当前已有的数据条数
-    # print(f"当前已有的数据条数: {len(historical_data)}")
-    
-    # 检查我们是否有足够的数据来计算滚动和EWM特征
-    if len(historical_data) >= max(rolling_windows['mean'], rolling_windows['std'], rolling_windows['rsi'], ewm_spans['long']):
-        # 在历史数据上计算滚动平均和标准差
-        historical_data['rolling_mean'] = historical_data['close'].rolling(window=rolling_windows['mean'], min_periods=1).mean()
-        historical_data['rolling_std'] = historical_data['close'].rolling(window=rolling_windows['std'], min_periods=1).std()
 
-        # 在历史数据上计算RSI
-        delta = historical_data['close'].diff()
-        gain = (delta.where(delta > 0, 0)).fillna(0)
-        loss = (-delta.where(delta < 0, 0)).fillna(0)
-        avg_gain = gain.rolling(window=rolling_windows['rsi'], min_periods=1).mean()
-        avg_loss = loss.rolling(window=rolling_windows['rsi'], min_periods=1).mean()
-        rs = avg_gain / avg_loss.replace(0, 1)  # 避免除以零
-        historical_data['RSI'] = 100 - (100 / (1 + rs))
+    # 检查是否有足够的数据来计算滚动和EWM特征
+    if len(historical_data) >= time_steps+20:
 
-        # 在历史数据上计算MACD
-        short_ema = historical_data['close'].ewm(span=ewm_spans['short'], adjust=False, min_periods=1).mean()
-        long_ema = historical_data['close'].ewm(span=ewm_spans['long'], adjust=False, min_periods=1).mean()
-        historical_data['MACD'] = short_ema - long_ema
-        historical_data['MACD_signal'] = historical_data['MACD'].ewm(span=ewm_spans['signal'], adjust=False, min_periods=1).mean()
 
-        # 将RSI和MACD移位以用作下一个时间步的特征
-        historical_data['RSI_shifted'] = historical_data['RSI'].shift(1)
-        historical_data['MACD_shifted'] = historical_data['MACD'].shift(1)
-        historical_data['MACD_signal_shifted'] = historical_data['MACD_signal'].shift(1)
+        for feature in price_features:
+            historical_data[feature + '_diff'] = historical_data['close'] - historical_data[feature]
+
+
+        historical_data['close_diff'] = historical_data['close'].diff()
+
+
+        data_for_scaling = historical_data[features].dropna()
+
+        # 选择最近的time_steps行用于归一化
+        data_to_scale = data_for_scaling.tail(time_steps)
+
+        # 归一化
+        scaled_data = scaler.fit_transform(data_to_scale)
         
-         #累积成交量
-        historical_data['volume_rolling_mean'] = historical_data['volume'].rolling(window=300).mean()
 
-        # 价格变化率和成交量变化
-        historical_data['price_change_rate'] = historical_data['close'].pct_change()
-        historical_data['volume_change'] = historical_data['volume'].diff()
+        # 使用归一化的数据创建模型输入
+        X_new = scaled_data.reshape(1, time_steps, len(features))
 
-        # 使用最后一行数据进行预测
-        X_new = historical_data.iloc[-1:][features]        
+
         # 检查X_new是否包含NaN值
-        if X_new.isnull().values.any():
-            # 处理包含NaN值的行（例如，跳过预测或使用占位符值）
-            # 例如，我们可以返回None或一个特定的信号表示数据不足
+        if np.isnan(X_new).any():
             return None, historical_data
         else:
             # 进行预测
-            prediction_proba = model.predict_proba(X_new)
-            # 获取预测为类别1的概率
-            probability_of_one = prediction_proba[0][1]
+            prediction_proba = model.predict(X_new,verbose=0)
+            probability_of_one = prediction_proba[0][0]
+
             return probability_of_one, historical_data
     else:
         # 数据不足以进行预测
         return None, historical_data
 
 
-rolling_windows = {'mean': 300, 'std': 300, 'rsi': 400}
-ewm_spans = {'short': 200, 'long': 800, 'signal': 800}
-
-
 # Initialize historical_data with the correct column names and types if necessary
 historical_data = pd.DataFrame()
-
 tick_count = 0
-
-
-
 while True:
     api.wait_update()
     # 判断整个tick序列是否有变化
     if api.is_changing(ticks):
         tick_count += 1
         tick = ticks.iloc[-1].to_dict()
-        probability, historical_data = predict_next_move(tick, model, rolling_windows, ewm_spans, historical_data)
+        quote = api.get_quote("SHFE.ss2312")
+        tick['pre_close'] = quote['pre_close']
+        tick['open'] = quote['open']
+        
+        # print(tick)
+        probability, historical_data = predict_next_move(tick, model, time_steps, historical_data,scaler)
         if probability is not None:
             print(f"预测为1的概率: {probability}")
-            buy_threshold = 0.9
-            sold_threshold = 0.6
+            buy_threshold = 0.8
+            sold_threshold = 0.4
             account = api.get_account()
             position = api.get_position("SHFE.ss2312")
-            quote = api.get_quote("SHFE.ss2312")
             target_pos = TargetPosTask(api, "SHFE.ss2312")
             if probability>buy_threshold and position.pos_long == 0:
-                price = tick['last_price']*5*0.13
+                price = tick['last_price']*quote['volume_multiple']*0.13
                 sim.set_margin("SHFE.ss2312", price)
                 volume = account.available // price
                 if volume > 0:
@@ -238,7 +228,7 @@ while True:
                                 log.write('\n') 
                             break
                         
-            elif position.pos_long >0 and tick_count>100 and probability<sold_threshold:
+            elif position.pos_long >0 and tick_count>30 and probability<sold_threshold:
                 target_pos.set_target_volume(0)
                 while True:
                     if position.pos_long == 0:
@@ -251,7 +241,6 @@ while True:
                                 log.write(str(quote))
                                 log.write('\n') 
                         break
-            print(tick)
             print("账户权益:%f, 账户余额:%f,持仓:%f" % (account.balance, account.available,position.pos_long))       
         else:
             # print(tick)
