7094724bf666099a4b5de3b41c72aa2472229426
清穹 <zark.zhangwb@alibaba-inc.com>
2024-01-23 13:03:05 +0800

    up

--- tianqin_test.py
+++ tianqin_test.py
@@ -1,7 +1,12 @@
 from tqsdk import TqAccount, TqApi, TqAuth,TqKq
-api = TqApi(TqAccount("H徽商期货", "952522", "Hello2023"), auth=TqAuth("卡卡罗特2023", "Hello2023"))
-klines = api.get_kline_serial("SHFE.ss2401", 1)
+# api = TqApi(TqAccount("H徽商期货", "952522", "Hello2023"), auth=TqAuth("卡卡罗特2023", "Hello2023"))
+api = TqApi(TqKq(), auth=TqAuth("卡卡罗特2023", "Hello2023"))
+ticks = api.get_tick_serial("SHFE.ss2401")
+quote = api.get_quote("SHFE.ss2401")
 while True:
     api.wait_update()
-    order = api.insert_order(symbol="SHFE.ss2401", direction="SELL", offset="CLOSETODAY", limit_price=14045, volume=1)
+    print(ticks)
+    print(quote)
+    
+
 
